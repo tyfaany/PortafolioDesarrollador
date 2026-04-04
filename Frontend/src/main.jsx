4c8e05065fa1c280260dd06d20c9b7d187b3d957
johnivansn <johnivansn@gmail.com>
2026-04-03 21:43:54 -0400

    FIX: importar AuthProvider y Bootstrap en main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.jsx'
 import './index.css'
+import { AuthProvider } from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <AuthProvider>
+    <AuthProvider>
       <App />
     </AuthProvider>
   </React.StrictMode>,
