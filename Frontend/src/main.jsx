8091ef13ab839f881816c720cd55b5c4c0719f35
johnivansn <johnivansn@gmail.com>
2026-04-09 21:27:05 -0400

    REFACTOR: mover App.css y actualizar import en main

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './assets/styles/App.css';
+import './styles/App.css';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import App from './App.jsx';
