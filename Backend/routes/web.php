9a4cc5c78573a4b0518c8faca82a0895e063cd16
johnivansn <johnivansn@gmail.com>
2026-05-23 16:45:53 -0400

    FEAT: habilitar rutas spa y rutas flexibles de almacenamiento

--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return ['Laravel' => app()->version()];
-});
-
-
+Route::get('/{any}', function () {
+    return view('index');
+})->where('any', '.*');
